f99cf8791ccd4d5b1d25517feb7093ef839e8757
namng <nam.nn@bidgear.com>
2023-12-26 00:52:39 +0700

    edit

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
 from seleniumbase import Driver
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 import time
 import uvicorn
-import threading
+import asyncio
 import socket
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",  # React app
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 class UserLogin(BaseModel):
@@ -15,25 +27,25 @@ class UserLogin(BaseModel):
     password: str
 
 
+two_factor_code_storage = {}
+
+
 # WebSocket route to receive messages from the socket server
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+
+@app.websocket("/ws/{username}")
+async def websocket_endpoint(websocket: WebSocket, username: str):
     await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        print(f"Received message from socket server: {message}")
-
-
-def get_message_from_socket(host: str, port: int):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(f"WebSocket connection accepted for {username}")
     try:
-        client_socket.connect((host, port))
         while True:
-            message = client_socket.recv(1024).decode()
-            print(f"Received message from socket server: {message}")
-            return message
+            data = await websocket.receive_text()
+            print(f"Received data from {username}: {data}")
+            two_factor_code_storage[username] = data
+            print(f"Stored 2FA code for {username}: {data}")
+    except WebSocketDisconnect:
+        print(f"WebSocket disconnected for {username}")
     finally:
-        client_socket.close()
+        await websocket.close()
 
 
 @app.post("/login")
@@ -59,50 +71,38 @@ async def login(user_login: UserLogin):
         a_button_element = driver.get_text(button)
         # Get the text of the <a> element with role="button"
         if a_button_element == "Need another way to confirm it's you?":
-            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = "0.0.0.0"  # Listen on all available network interfaces
-            port = 12345  # Adjust the port as needed
+            start_time = time.time()
+            username = user_login.username  # Assuming this is the unique identifier
 
-            server_socket.bind((host, port))
-            server_socket.listen(5)
+            # Wait for the 2FA code to be received via WebSocket
+            while username not in two_factor_code_storage:
+                await asyncio.sleep(1)  # Non-blocking sleep
+                # Implement a timeout mechanism
+                if time.time() - start_time > 30:  # 30 seconds timeout
+                    return {"error": "Timeout waiting for 2FA code"}
+            two_factor_code = two_factor_code_storage.pop(username)
 
-            print(f"Socket server is listening on {host}:{port}")
-
-            while True:
-                client_socket, addr = server_socket.accept()
-                print(f"Accepted connection from {addr}")
-
-                message = client_socket.recv(1024).decode()
-                print(f"Received message from client: {message}")
-
-                driver.type("#approvals_code", message)
+            driver.type("#approvals_code", two_factor_code)
+            time.sleep(2)
+            while driver.is_element_visible('#checkpointSubmitButton'):
+                driver.click('#checkpointSubmitButton')
                 time.sleep(2)
-                driver.click('#checkpointSubmitButton')
-                time.sleep(5)
-                driver.click('#checkpointSubmitButton')
-                client_socket.close()
-                break
-
         time.sleep(10)
         cookies = driver.get_cookies()
         cookie_string = ';'.join([f"{cookie['name']}:{cookie['value']}" for cookie in cookies])
-
-        return {"cookie": cookie_string}
-    return {"cookie": ''}
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        message = await websocket.receive_text()
-        print(f"Received message from socket server: {message}")
+        data = {
+            'cookie': cookie_string
+        }
+        print(data)
+        driver.quit()
+        return JSONResponse(content=data)
+    return JSONResponse(content={})
 
 
 # Function to handle incoming socket connections and messages
 
 def main():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
 
 
 if __name__ == "__main__":
